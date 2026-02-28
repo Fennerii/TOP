d17dce5a0b203a2f3381da8b3239c7cd6392e694
fennerii <jonathanfennerii@gmail.com>
2026-02-28 16:19:27 -0500

    reformat

---
+++ FreeCodeCamp/Javascript /Build-a-Sentence-Maker.js
@@ -0,0 +1,33 @@
+// Declare variables using let
+let adjective;
+let noun;
+let verb;
+let place;
+let adjective2;
+let noun2;
+
+// Assign initial values
+adjective = 'handsome';
+noun = 'man';
+verb = 'pressed';
+place = 'Indonesia';
+adjective2 = 'beautiful';
+noun2 = 'spaghetti';
+
+// First story (ONE LINE)
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("First story: " + firstStory);
+
+// Assign new values
+adjective = 'pretty';
+noun = 'woman';
+verb = 'touched';
+place = 'Malaysia';
+adjective2 = 'awesome';
+noun2 = 'nasi kucing';
+
+// Second story (ONE LINE)
+let secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("Second story: " + secondStory);
